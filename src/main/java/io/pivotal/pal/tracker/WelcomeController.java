f352b728b9ee6a5fc28eafb0cbb04744fc61c5d0
Srinivas Kamsani <srinivas.kamsani@perficient.com>
2018-04-10 10:19:53 -0600

    Implemeted env and manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,29 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    public String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+          return getMessage();
     }
 }
